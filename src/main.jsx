71f7c5b88476878a28b77f86334af50c2115b672
aldogerard <aldogerard12@gmail.com>
2024-11-20 17:04:20 +0700

    feat : login on auth slice

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import "@/styles/global.css";
 import App from "@/App";
 import { Provider } from "react-redux";
 import store from "@/redux/store";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <QueryClientProvider client={queryClient}>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </QueryClientProvider>
     </StrictMode>
 );
--- src/redux/features/authSlice.js
+++ src/redux/features/authSlice.js
@@ -1,5 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { axiosInstance } from "@/lib/axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { jwtDecode } from "jwt-decode";
+
+export const login = createAsyncThunk(
+    "auth/login",
+    async ({ username, password }) => {
+        const { data } = await axiosInstance.post("auth/login", {
+            username,
+            password,
+        });
+        return data;
+    }
+);
 
 const loadFromLocalStorage = () => {
     try {
@@ -11,30 +23,39 @@ const loadFromLocalStorage = () => {
     }
 };
 
-const { user, token } = loadFromLocalStorage();
-
 const authSlice = createSlice({
     name: "auth",
     initialState: {
-        user: user || null,
-        token: token || null,
+        user: loadFromLocalStorage().user || null,
+        token: loadFromLocalStorage().token || null,
     },
     reducers: {
         setUser(state, action) {
+            console.log(action.payload);
             const { user, token } = action.payload;
             state.user = user;
             state.token = token;
 
             localStorage.setItem("user", JSON.stringify(user));
-            localStorage.setItem("authToken", token);
+            localStorage.setItem("token", token);
         },
         clearUser(state) {
             state.user = null;
             state.token = null;
 
             localStorage.removeItem("user");
-            localStorage.removeItem("authToken");
+            localStorage.removeItem("token");
         },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(login.fulfilled, (state, action) => {
+            const { accessToken, email, id } = action.payload;
+            state.user = { email, id };
+            state.token = accessToken;
+
+            localStorage.setItem("user", JSON.stringify({ email, id }));
+            localStorage.setItem("token", accessToken);
+        });
     },
 });
 
